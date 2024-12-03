7872f860e3c1d788514aa180cf253f00b4fb3884
EscribanoC <escribano.flores.carlos@iescamas.es>
2024-12-03 11:39:09 +0100

    Cambios en la configuracion de la BBDD

--- models/Database.php
+++ models/Database.php
@@ -4,9 +4,9 @@ class Database{
 
     public function connect(){
         try{
-            return new PDO('mysql:host=localhost;dbname=crud_ejemplo;charset=utf8;',
-                'root',
-                '',
+            return new PDO('mysql:host=localhost;dbname=carlosescribanoexamenmvc;charset=utf8;',
+                'mycarlos05',
+                'DgL6ftS4',
                 [
                     /**
                      * Activar el manejo de errores y retornar una exception.
